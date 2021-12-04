417b7d2e6e7eff20292c5429cbe5245813e9a1d9
Ken Trinh <epicness.pandas@gmail.com>
2021-12-04 12:19:05 -0500

    hw3

--- Assignment_3/marbles.py
+++ Assignment_3/marbles.py
@@ -1,6 +1,7 @@
 # A Marbles Game
 # A Marbles Game - Problem
 # Cuong Trinh
+import sys
 
 class MarblesBoard():
     def __init__(self, board):
@@ -75,9 +76,36 @@ class Solver():
         print(str(self))
 
 
+def main(sequence):
+    board = MarblesBoard(sequence)
+    player = Solver(board)
+    player.solve()
 
 
 if __name__ == "__main__":
-    board = MarblesBoard((3, 6, 7, 4, 1, 0, 8, 2, 5))
-    player = Solver(board)
-    player.solve()
+    """
+        Big O Complexity for this Algorithm (Logic/Deduction):
+        1) The Algorithm repeatedly call onto switch and/or rotate method until the list is sorted
+        2) At each switch/rotate call, the algorithm perform a check of the current list against to see if it is sorted
+            - To do this, we can either compared the current list with a sorted list, which the best case for the quickest sorting algorithm is O(NlogN), worst is O(N^2)
+            - Or we can iterate through the current list to check if item(i - 1) < item(i), which the worst case is O(N)
+        3) Each time the list is not sorted, we only perform 1 check between the first two elements
+            - In other words, each one of this check is O(1) - constant time
+            - Each switch is O(1)
+            - Each rotate O(N) since every other element follow element 0 has to be shifted
+            - assuming the worst case O(N)
+        4) At any given state of the board, the algorithm is essentially performing:
+            - (N*N + N*N + N*N + ......) = a*N^2, where a is all real numbers and only rotate is performed
+            - (1*N + 1*N + 1*N + ......) = a*N, where a is all real numbers and only switch is performed
+
+        Deduction:
+            - Therefore, the algorithm has a lower bound of O(N) and an upper bound of O(N^2) in term of time complexity
+            Ex: O(N): 
+                given: [2,1]
+                perform 1 switch and 1 check => (O(1*N)) = O(N)
+                
+    """
+
+    sequence = tuple(sys.argv[1].split(","))
+    sequence = [int(i) for i in sequence]
+    main(sequence)
